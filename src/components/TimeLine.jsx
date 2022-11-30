1f3fcf05c95921c2daf3f5cfd8f9783530057b5e
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2022-12-01 01:40:54 +0200

    timeline outline

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -1,5 +1,21 @@
+import timeline from "../data/timeline";
+import TimeLineItem from "./TimeLineItem";
+
 function TimeLine() {
-  return <section>Timeline</section>;
+  return (
+    <section className="flex flex-col md:flex-row justify-center my-20">
+      <div className="px-4 w-full md:w-7/12">
+        {timeline.map((event) => (
+          <TimeLineItem
+            year={event.year}
+            title={event.title}
+            duration={event.duration}
+            details={event.details}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default TimeLine;
---
+++ src/data/timeline.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    year: "Today",
+    title: "Physics Bachelor's Degree",
+    duration: "4 years",
+    details:
+      "My passion for understanding what constitutes reality " +
+      "and how humans can influence lead me to major in Physics. " +
+      "My 4 years of experience and interaction with students and " +
+      "teachers ensured me that Computer Science is my passion.",
+  },
+  {
+    year: "Today",
+    title: "Human Resources Manager in Young Minds NKUA",
+    duration: "1 year",
+    details:
+      "Contribute as a Volunteer in Young Minds NKUA Section with my fellow students and friends. " +
+      "The main reason I choose to be in that positions is that I warmly support the three main goals of " +
+      "Young Minds (education, networking and outreach). Also, I hone my skills in organization of teams, " +
+      "leadership and communication.",
+  },
+  {
+    year: "2022",
+    title: "Internship at 7L International",
+    duration: "2 months",
+    details:
+      "Worked as software engineer, specialized in Web Development, " +
+      "at 7L International. At first, I took two Udemy Courses about " +
+      "React.js and Next.js. Also, I built REST APIs with Next.js and " +
+      "used them to make the Web Apps interact with a Database. Alongside, " +
+      "I learned how to work in a team with Git and Git management systems (GitLab, GitHub). " +
+      "The last three weeks I prepared a Webinar for the IT Team about the " +
+      "newly developed framework, Qwik.js, and Remix.js, which is thought as " +
+      "a strong opponent to Next, comparing them. For the sake of the Webinar I " +
+      "also developed a Qwik app for showcase.",
+  },
+];
