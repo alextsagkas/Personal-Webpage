0ead1f143bb6c232ed0c08be81ad0dfc13ffa8fc
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2022-11-30 21:19:42 +0200

    introduction

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,5 +1,25 @@
 function Introduction() {
-  return <section>Introduction</section>;
+  return (
+    <section
+      className="flex items-center justify-center flex-col text-center
+                pt-20 pb-6"
+    >
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
+        Alexandros Tsagkaropoulos
+      </h1>
+      <p className="text-base md:text-xl mb-3 font-medium">
+        Physics Student & Developer
+      </p>
+      <p className="text-sm max-w-xl mb-6 font-bold">
+        I'm a Bachelor Physics Student trying to figure out the world we live
+        in. I specialize in Electronics and Computers and I seek to apply my
+        knowledge to Software Development Projects.
+        <br />
+        All Software Engineering Projects are carefully outlined, designed and
+        implemented to solve real world problems efficiently.
+      </p>
+    </section>
+  );
 }
 
 export default Introduction;
