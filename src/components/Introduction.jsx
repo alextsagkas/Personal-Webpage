153718db41e0773dd071bd19843ce7637b1ec889
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2022-12-01 22:25:03 +0200

    dark mode

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -4,7 +4,10 @@ function Introduction() {
       className="flex items-center justify-center flex-col text-center
                 pt-20 pb-6"
     >
-      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
+      <h1
+        className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold
+      dark:text-white"
+      >
         Alexandros Tsagkaropoulos
       </h1>
       <p className="text-base md:text-xl mb-3 font-medium">
@@ -13,10 +16,9 @@ function Introduction() {
       <p className="text-sm max-w-xl mb-6 font-bold">
         I'm a Bachelor Physics Student trying to figure out the world we live
         in. I specialize in Electronics and Computers and I seek to apply my
-        knowledge to Software Development Projects.
-        <br />
-        All Software Engineering Projects are carefully outlined, designed and
-        implemented to solve real world problems efficiently.
+        knowledge to Software Development Projects. All Software Engineering
+        Projects are carefully outlined, designed and implemented to solve real
+        world problems efficiently.
       </p>
     </section>
   );
--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,30 +1,40 @@
 function PortfolioItem({ title, imgUrl, stack, link }) {
   return (
-    <section className="border-2 border-stone-900 rounded-md overflow-hidden">
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="border-2 border-stone-900 rounded-md overflow-hidden
+    dark:border-white"
+    >
       <img
         src={imgUrl}
         alt="portfolio-img"
         className="w-full h-36 m:h-48 object-cover cursor-pointer "
       />
       <div className="w-full p-4">
-        <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
+        <h3
+          className="text-lg md:text-xl mb-2 md:mb-3 font-semibold
+        dark:text-white"
+        >
           {title}
         </h3>
         <p
           className="flex felx-wrap gap-2 flex-row items-center justify-start 
-                    text-xs md:text-sm"
+                    text-xs md:text-sm dark:text-white"
         >
           {stack.map((item) => (
             <span
               className="inline-block px-2 py-1 font-semibold border-2 
-              border-stone-900 rounded-md"
+              border-stone-900 rounded-md
+              dark:border-white"
             >
               {item}
             </span>
           ))}
         </p>
       </div>
-    </section>
+    </a>
   );
 }
 
