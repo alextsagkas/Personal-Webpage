b2d8862ae0597e7cc4c9bc8ee372f75aa4e12949
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2022-12-01 23:04:34 +0200

    change color in contact button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,7 +33,7 @@ function Contact() {
             type="button"
             className="text-center inline-block px-8 py-3 w-max text-base 
             font-medium rounded-md text-white bg-gradient-to-r 
-            from-yellow-500 to-pink-500 drop-shadow-md 
+            from-blue-500 to-violet-400 dark:to-orange-400 drop-shadow-md 
             hover: stroke-white"
           >
             Work with Me
