a294e20ff15919cd211af3c53db849d307dd7b4d
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2022-12-01 21:24:55 +0200

    footer outline

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
 function Footer() {
-  return <section>Footer</section>;
+  return (
+    <section className="py-5 text-center ">
+      {/* TODO: Social Icons */}
+      <p className="text-sm mt-2 opacity-50">
+        &copy; {new Date().getFullYear()} Alexandros Tsagkaropoulos. All rights
+        reserved.
+      </p>
+    </section>
+  );
 }
 
 export default Footer;
