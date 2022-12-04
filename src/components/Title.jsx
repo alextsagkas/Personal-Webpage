70ede04b3336308d6536744bc0b34c434d8e37e2
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2022-12-04 13:22:26 +0200

    bgDark color

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ function Title({ children, id }) {
   return (
     <h1
       id={id & id}
-      className="mb-5 text-2xl font-bold text-stone-900 underline decoration-4 underline-offset-8 dark:text-white"
+      className="mb-5 text-2xl font-bold text-bgDark-900 underline decoration-4 underline-offset-8 dark:text-white"
     >
       {children}
     </h1>
