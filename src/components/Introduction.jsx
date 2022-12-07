d9fdc97b89ac88e1e7002a12c005170b92406649
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2022-12-08 00:56:10 +0200

    padding in introduction div

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -5,16 +5,16 @@ const Introduction = forwardRef(({ toolBarHidden }, ref) => {
   return (
     <section
       ref={ref}
-      className="flex h-screen w-screen flex-col items-center justify-center px-2 text-center"
+      className="flex h-screen w-screen flex-col items-center justify-center text-center"
     >
       <div className="h-full" />
-      <h1 className="mb-2 text-4xl font-bold dark:text-white md:mb-3 md:text-7xl">
+      <h1 className="mb-2 px-7 text-4xl font-bold dark:text-white md:mb-3 md:text-7xl">
         Alexandros Tsagkaropoulos
       </h1>
       <p className="mb-6 text-base font-semibold md:text-xl">
         Physics Student & Developer
       </p>
-      <p className="mb-6 max-w-xl text-sm font-medium">
+      <p className="mb-6 max-w-xl px-7 text-sm font-medium">
         I'm a Bachelor Physics Student trying to figure out the world we live
         in. I specialize in Electronics and Computers and I seek to apply my
         knowledge to Software Development Projects. All Software Engineering
