a388d3ea6f3da6b330bb4cd64f2b273dc0b66b5c
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2022-12-08 01:40:15 +0200

    custom breakpoints Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,8 @@ function NavBar({ theme, themeSwitcherHandler, removePageHandler, refs }) {
   const { portfolioRef, timeLineRef, contactRef } = refs;
 
   return (
-    <nav className="fixed top-0 z-10 h-14 w-full border-b-[0.6px] border-bgDark-400 bg-bgLight-50 py-3  text-sm text-bgDark-900 opacity-90 dark:bg-bgDark-900 dark:text-white smallPhone:text-base">
-      <ul className="mx-auto flex h-full w-full max-w-5xl list-none flex-row items-center justify-center gap-4 px-4 font-medium navbarPadding:w-11/12 smallPhone:gap-5 md:w-8/12">
+    <nav className="min-[380px]:text-base fixed top-0 z-10 h-14 w-full border-b-[0.6px] border-bgDark-400 bg-bgLight-50 py-3 text-sm text-bgDark-900 opacity-90 dark:bg-bgDark-900 dark:text-white">
+      <ul className="min-[360px]:w-11/12 min-[380px]:gap-5 mx-auto flex h-full w-full max-w-5xl list-none flex-row items-center justify-center gap-4 px-4 font-medium md:w-8/12">
         <button
           onClick={() => {
             removePageHandler(false);
@@ -34,7 +34,7 @@ function NavBar({ theme, themeSwitcherHandler, removePageHandler, refs }) {
         >
           Contact
         </button>
-        <li className="hidden w-full navbar:inline-block"></li>
+        <li className="min-[400px]:inline-block hidden w-full"></li>
         <ThemeButton
           theme={theme}
           themeSwitcherHandler={themeSwitcherHandler}
