8b682582e7a489791c2941efc4e2bb938653d110
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2022-12-08 14:09:59 +0200

    rearrange Contact Components

--- src/components/helpers/Input.jsx
+++ src/components/helpers/Input.jsx
@@ -2,7 +2,7 @@ function Input({ text, type }) {
   return (
     <input
       type={type}
-      name={text}
+      name={text.toLowerCase()}
       placeholder={text}
       className="rounded-md border-2 border-bgDark-300 bg-transparent p-2 caret-violet-500 selection:bg-violet-400 focus:border-violet-400 focus:outline-none dark:caret-orange-400 dark:selection:bg-orange-300 dark:selection:text-bgDark-900 dark:focus:border-bgLight-200"
       required
