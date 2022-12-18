6d845a1e191499f05783f8a7641b31e22ce2b816
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2022-12-19 00:01:35 +0200

    deploy.sh modification - add master branch

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:alextsagkas/Personal-Website.git main:gh-pages
+git push -f git@github.com:alextsagkas/Personal-Website.git master:gh-pages
 
 
 cd -
